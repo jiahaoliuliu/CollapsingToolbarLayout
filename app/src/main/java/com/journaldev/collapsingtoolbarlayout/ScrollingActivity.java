feca09bded0824f2e4d91c900880ce1c90fe7ddb
Jiahao Liu <jiahao.liu@dnata.com>
2020-02-26 15:04:29 +0400

    First version of the app (functional)

--- app/src/main/java/com/journaldev/collapsingtoolbarlayout/ScrollingActivity.java
+++ app/src/main/java/com/journaldev/collapsingtoolbarlayout/ScrollingActivity.java
@@ -1,14 +1,16 @@
 package com.journaldev.collapsingtoolbarlayout;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ScrollingActivity extends AppCompatActivity {
 
